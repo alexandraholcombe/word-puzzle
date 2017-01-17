3288823241adc2fb29bdab4657e25d85d932882c
Alexandra Holcombe and Minh Phuong <minh@>
2017-01-17 11:05:40 -0800

    Add document.ready, add array with vowels

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//Business Logic
+var vowels = ["a", "i", "e", "o", "u"];
+//User Interface
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+  });
+});
