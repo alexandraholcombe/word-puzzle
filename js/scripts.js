03a14ce15597e9cba1388f429583e80deafa4404
Alexandra Holcombe and Minh Phuong <alexandra@>
2017-01-17 11:57:25 -0800

    separate business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,28 @@ var userInput = 0;
 var stringCharacters = 0;
 var outputString = 0;
 
+var replaceVowels = function(){
+  userInput = $("input#user-input").val();
+
+  stringCharacters = userInput.split("");
+
+  for (var character=0; character<stringCharacters.length; character++) {
+    vowels.forEach(function(vowel) {
+      if (stringCharacters[character] === vowel) {
+        stringCharacters.splice(character, 1, "-");
+        console.log(stringCharacters);
+      };
+    });
+  }
+  outputString = stringCharacters.join("");
+  console.log(outputString);
+};
+
 //User Interface
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    replaceVowels();
 
-    userInput = $("input#user-input").val();
-    console.log(userInput);
-
-    stringCharacters = userInput.split("");
-    console.log(stringCharacters);
-
-    for (var character=0; character<stringCharacters.length; character++) {
-      vowels.forEach(function(vowel) {
-        if (stringCharacters[character] === vowel) {
-          stringCharacters.splice(character, 1, "-");
-          console.log(stringCharacters);
-        };
-      });
-    }
-    // stringCharacters.join("");
-    console.log(stringCharacters);
-    outputString = stringCharacters.join("");
-    console.log(outputString);
   });
 });
