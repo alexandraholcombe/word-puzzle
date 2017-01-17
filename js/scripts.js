a7a5dbcce12af708a70cfad37bea2575d8bbb171
Alexandra Holcombe and Minh Phuong <minh@>
2017-01-17 11:09:32 -0800

    Split string into array stringCharacters

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 //Business Logic
 var vowels = ["a", "i", "e", "o", "u"];
+var userInput = 0;
+var stringCharacters = 0;
+
 //User Interface
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
+    userInput = $("input#user-input").val();
+    console.log(userInput);
+
+    stringCharacters = userInput.split("");
+
+    console.log(stringCharacters);
   });
 });
